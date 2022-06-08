f27ee99220478cc774f31cff7146985360d6826b
Elijah Rippeth <elijah.rippeth@gmail.com>
2022-06-08 18:00:34 -0400

    fix tiny bugs.

--- spm_filter/filter.py
+++ spm_filter/filter.py
@@ -1,15 +1,15 @@
 import sentencepiece as spm
+from itertools import islice
 
 def buffer(l, buffer_size):
-    l = []
-    for i in range(buffer_size):
-        l.append(i)
-    return l
+    return list(islice(l, buffer_size))
 
 def spm_filter(model, lines, min_len=0, max_len=256, buffer_size=1024):
     while lines:
         out = []
         batch = buffer(lines, buffer_size)
+        if not batch:
+            return
         encoded = model.Encode(batch)
         for line, e in zip(batch, encoded):
             if min_len <= len(e) <= max_len:
